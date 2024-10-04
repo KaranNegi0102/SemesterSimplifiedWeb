b5d4d56d8253da681b2cdea0ffca0212e255120a
KaranNegi0102 <karannegi0102@gmail.com>
2024-10-05 00:13:45 +0530

    created userMOdel

---
+++ backend/models/userModel.js
@@ -0,0 +1,14 @@
+
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    title: { type: String, required: false },
+    role: { type: String, required: false },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, required: true, default: false },
+    tasks: [{ type: Schema.Types.ObjectId, ref: "Task" }],
+    isActive: { type: Boolean, required: true, default: true },
+  },
+  { timestamps: true }
+);
